66ed6bcabdf65e0ea61d3ec7a2c7cdca9862f15e
Tim Andersen <tim@spreadsheetgear.com>
2023-02-08 16:14:03 -0600

    Add MyLibEng submodule, MyLibWin and vscode support

---
+++ MyLibWin/MyLibWin.cs
@@ -0,0 +1,11 @@
+﻿namespace MyLibWin
+{
+    public class MyLibWin
+    {
+        public string DoSomethingWin()
+        {
+            var libEng = new MyLibEng.MyLibEng();
+            return $"MyLibEng.DoSomethingWin() was called => {libEng.DoSomethingEng()}";
+        }
+    }
+}
